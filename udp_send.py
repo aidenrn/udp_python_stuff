5c7059eaf9ae9e2f5441f2b14cb74002edbdf6c8
me <aidenrn@gmail.com>
2016-04-20 02:58:40 +0100

    first working version

--- udp_send.py
+++ udp_send.py
@@ -13,7 +13,9 @@ UDP_IP =input("What is the ip?")
 UDP_PORT= input("What is the port number?")
 MESSAGE=input("What is the message?")
 
+print(UDP_IP,type(UDP_IP))
+print(UDP_PORT,type(UDP_PORT))
+print(MESSAGE,type(MESSAGE))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-sock.sendto(MESSAGE.encode(), (UDP_IP,int( UDP_PORT)))
+sock.sendto(MESSAGE.encode(), (UDP_IP,int(UDP_PORT)))
